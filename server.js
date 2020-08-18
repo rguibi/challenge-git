d32ba24ef860721046dbb5c76183b1b982dbd744
rguibi <rguibi.arjdal@outlook.fr>
2020-08-18 15:32:28 +0100

    chalenge

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http')
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') return respondHello(req, res)
+
+  res.end()
+})
+
+function respondHello (req, res) {
+  res.end(JSON.stringify({ msg: 'hello' }))
+}
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
+}
+
+function respondBase64 (req, res) {
+  const phrase = req.url.replace(/^\/b64\//, '')
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
+}
+
+server.listen(PORT)
+console.log(`Server listening on port ${PORT}`)
+
+if (require.main !== module) module.exports = server
